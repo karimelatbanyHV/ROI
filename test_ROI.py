7da88025d039e44782c3a5f4cd1744362817aa9a
Karim_ElAtbany <152432663+karimelatbanyHV@users.noreply.github.com>
2024-07-10 15:26:29 +0300

    q

---
+++ test_ROI.py
@@ -0,0 +1,10 @@
+import ROI
+import cv2
+
+
+rois = ROI.select_rois('last.mp4')
+print("Stored ROIs:")
+for roi in rois:
+    print(f"ID: {roi['id']}, Coordinates: {roi['coordinates']}")
+    
+    cv2.imwrite(f"{roi['id']}.jpg", roi['cropped_image'])
